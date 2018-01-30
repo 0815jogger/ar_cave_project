4ce5e0eaf22959339cde326c88efed3b1b22ed2e
Pascal Helbig <pascalhelbig@gmail.com>
2018-01-30 19:40:39 +0100

    scale verlangsamt

--- Assets/arCaveCode/arCaveBehaviours/arCaveScaleBehaviour.cs
+++ Assets/arCaveCode/arCaveBehaviours/arCaveScaleBehaviour.cs
@@ -6,9 +6,9 @@ public class arCaveScaleBehaviour : MonoBehaviour
     // Update is called once per frame
     public void CaveScale(GameObject gameObject, Body body)
     {
-        float sizex = (float)(body.Joints[JointType.HandRight].Position.X * 0.1);
-        float sizey = (float)(body.Joints[JointType.HandRight].Position.Y * 0.1);
-        float sizez = (float)(body.Joints[JointType.HandRight].Position.Z * 0.1);
+        float sizex = (float)(body.Joints[JointType.HandRight].Position.X * 0.05);
+        float sizey = (float)(body.Joints[JointType.HandRight].Position.Y * 0.05);
+        float sizez = (float)(body.Joints[JointType.HandRight].Position.Z * 0.05);
 
         gameObject.transform.localScale = new Vector3
             (
