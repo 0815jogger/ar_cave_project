dab08d10ef9a46b9ba2155361db167c9a1ca5a3a
Pascal Helbig <pascalhelbig@gmail.com>
2018-01-30 20:29:21 +0100

    rotate verbessert

--- Assets/arCaveCode/arCaveBehaviours/arCaveRotateBehaviour.cs
+++ Assets/arCaveCode/arCaveBehaviours/arCaveRotateBehaviour.cs
@@ -5,15 +5,15 @@ public class arCaveRotateBehaviour : MonoBehaviour
 {
     public void CaveRotate(GameObject gameObject, Body body)
     {
-        float anglex = (float)(body.Joints[JointType.HandRight].Position.X);
-        float angley = (float)(body.Joints[JointType.HandRight].Position.Y);
-        float anglez = (float)(body.Joints[JointType.HandRight].Position.Z);
+        float anglex = (float)(body.Joints[JointType.HandRight].Position.Y);
+        float angley = - (float)(body.Joints[JointType.HandRight].Position.X);
+        float anglez = 0; // (float)(body.Joints[JointType.HandRight].Position.X);
 
         gameObject.transform.rotation = Quaternion.Euler
             (
-                gameObject.transform.rotation.x + anglex * 100,
-                gameObject.transform.rotation.y + angley * 100,
-                gameObject.transform.rotation.z + anglez * 100
+                gameObject.transform.rotation.x + anglex * 200,
+                gameObject.transform.rotation.y + angley * 200,
+                gameObject.transform.rotation.z + anglez * 200
             );
     }
 }
